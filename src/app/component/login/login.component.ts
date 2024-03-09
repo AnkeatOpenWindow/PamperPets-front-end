28942decc316e59f230d201a0e2640cefca87ed0
AnkeatOpenWindow <221202@virtualwindow.co.za>
2024-03-09 19:02:55 +0200

    login and crafting

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { InventoryItemComponent } from '../cards/inventory-item/inventory-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ InventoryItemComponent, ReactiveFormsModule , FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  hide = true;
 }
