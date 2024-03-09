da9049966e614abbd3023d016da91750f7d650b1
AnkeatOpenWindow <221202@virtualwindow.co.za>
2024-03-09 17:02:40 +0200

    routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { Routes } from '@angular/router';
 import { inventoryComponent } from './component/inventory/inventory.component';
 import { CraftingComponent } from './component/crafting/crafting.component';
 import { PagenotfountComponent } from './component/pagenotfount/pagenotfount.component';
+import { LandingComponent } from './component/landing/landing.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ChartsComponent } from './component/charts/charts.component';
+import { LoginComponent } from './component/login/login.component';
 // Define your routs here but keep them in order.
 export const routes: Routes = [
+    {path:"landing", component: LandingComponent},
+    {path:"login", component: LoginComponent},
+    {path:"dashboard", component: DashboardComponent},
     {path:"inventory", component: inventoryComponent},
     {path:"crafting", component: CraftingComponent},
-    {path:"", redirectTo:"inventory", pathMatch:"full"}, //taking us to our first path
+    {path:"chart", component: ChartsComponent},
+    {path:"", redirectTo:"landing", pathMatch:"full"}, //taking us to our first path
     {path:"**", component: PagenotfountComponent}, //wild card
     
 ];
